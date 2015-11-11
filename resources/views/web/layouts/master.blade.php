729cf109fc219f1bcf281dd160cde5c774444975
Adrián Pardellas Blunier <adrian@anavallasuiza.com>
2015-11-11 11:42:39 +0100

    engadido kitchen sink

---
+++ resources/views/web/layouts/master.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<!--[if IE 9]><html class="lt-ie10 no-js" lang="{{App::getLocale()}}"><![endif]-->
+<html class="no-js" lang="{{ App::getLocale() }}" data-url="{{ asset('/') }}">
+    <head>
+        {{--@section('head')
+            @include('web.molecules.head.default')
+        @show--}}
+    </head>
+
+    <body id="top" class="@section('body-classes')@show">
+
+            {{--@section('header')
+                @include('web.molecules.header.default')
+            @show--}}
+
+            {{--@section('flash')
+                @include('web.atoms.alerts.flash')
+            @show--}}
+
+            {{--@section('errors')
+                @include('web.atoms.alerts.errors')
+            @show--}}
+
+            @yield('content')
+
+            {{--@section('footer')
+                @include('web.molecules.footer.default')
+            @show--}}
+
+        {!! HtmlHelper::jsModule('common') !!}
+
+        @section('footer-scripts')
+        {!! HtmlHelper::jsModule('main') !!}
+        @show
+    </body>
+</html>
