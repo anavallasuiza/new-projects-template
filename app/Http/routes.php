0eb468b062fc922ba0bd771b02be362e53c7a816
Carlos Morales <carlos@anavallasuiza.com>
2015-11-12 18:43:51 +0100

    Eliminada ruta

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,9 +1,4 @@
 <?php
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 if (config('app.debug')) {
     Route::get('/kitchen-sink', function () {
         return view('web.kitchen-sink');
