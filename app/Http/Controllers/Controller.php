14d26d76a2e81254daa828d4a5f68f2fc2809562
Carlos Morales <carlos@anavallasuiza.com>
2015-11-12 18:48:03 +0100

    Meta

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,15 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Meta;
 
 abstract class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function __construct()
+    {
+        Meta::meta('title', __('meta-title-default'));
+        Meta::meta('description', __('meta-description-default'));
+    }
 }
