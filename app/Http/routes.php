feb332e587065c40359f991afe4882b0ecac5c18
Adrián Pardellas Blunier <adrian@anavallasuiza.com>
2015-11-13 19:12:55 +0100

    welcome route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,6 +1,10 @@
 <?php
 if (config('app.debug')) {
-    Route::get('/kitchen-sink', function () {
+    Route::get('/kitchen-sink', ['as' => 'kitchen-sink', 'uses' => function () {
         return view('web.kitchen-sink');
-    });
+    }]);
 }
+
+Route::get('/', ['as' => 'welcome', 'uses' => function () {
+    return view('welcome');
+}]);
