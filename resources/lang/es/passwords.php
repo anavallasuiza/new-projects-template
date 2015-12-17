1a07bfd8a5085373d34bdc8293d360dc59238b31
Carlos Morales <carlos@anavallasuiza.com>
2015-12-17 11:25:24 +0100

    Traducións básicas en castelán

---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return array (
+  'password' => 'La contraseña debe contener al menos 6 caracteres y ser igual a la confirmación.',
+  'reset' => 'Tu contraseña ha sido actualizada.',
+  'sent' => 'Te hemos enviado un enlace para recuperar tu contraseña. ',
+  'token' => 'El token es incorrecto.',
+  'user' => 'No hemos podido encontrar ningún usuario con ese email.',
+);
