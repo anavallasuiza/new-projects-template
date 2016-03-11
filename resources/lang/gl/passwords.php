fc848522bd9917c750734d7fd8498f536cb68ae9
Adrián Pardellas Blunier <adrian@anavallasuiza.com>
2016-03-11 18:03:32 +0100

    added gallician translations

---
+++ resources/lang/gl/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return array (
+  'password' => 'O contrasinal debe conter polo menos 6 caracteres e ser igual á confirmación.',
+  'reset' => 'O teu contrasinal foi actualizado.',
+  'sent' => 'Enviámosche unha ligazón para recuperar o teu contrasinal. ',
+  'token' => 'O token é incorrecto.',
+  'user' => 'Non puidemos atopar ningún/ha usuario/a con este email.',
+);
